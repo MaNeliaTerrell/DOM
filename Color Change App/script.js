a119997cb9df59888f3be42b6a7d7e24132eb7b2
MaNeliaTerrell <nterrell1014@gmail.com>
2022-09-17 08:16:23 -0400

    DOM

--- Color Change App/script.js
+++ Color Change App/script.js
@@ -1,57 +1,48 @@
 const COLORS_ARRAY = [
-    "red",
-    "yellow",
-    "magenta",
-    "cyan",
-    "blue",
-    "black",
-    "gray",
-    "teal",
-    "green",
-    "purple",
-    "violet",
+  "red",
+  "yellow",
+  "magenta",
+  "cyan",
+  "blue",
+  "black",
+  "gray",
+  "teal",
+  "green",
+  "purple",
+  "violet",
 ];
 
-
 // ===== CHALLENGE =====
-      // 1. Create A Function (generate)
-      // Function should get a random color from the COLORS_ARRAY
-    const randomColors = Math.floor(Math.random() * COLORS_ARRAY.length);
-
-    // Grab ul, body, button from HTML
-
-    const body = document.querySelector('body')
-    const button = document.getElementById("#generate-color")
-    const ul = document.getElementById('#color-list')
-
-    // Event listener for button
-      
-    button.addEventListener('click',function(e){
-        console.log(e.target);)
-    li.addEventListener('click',function(e){
-        console.log(e.target);)
-      // Create a li element
-    const li = document.createElement('li')
-
-    // colors on the li element  
-    // Add the color name to the li text
-    li.textContent = randomColors
- 
-    //    Set the background color of the li to the random colors
-
-    body.style.backgroundColor = randomColors 
-
-      // Append the li to the ul
-
-    ul.appendChild(li)
-     
-    //  
+// 1. Create A Function (generate)
+// Function should get a random color from the COLORS_ARRAY
+// Create a li element
+// Add the color name to the li text
+// Set the background color of the li to the random color
+// Append the li to the ul
 // 2. Create Function (reset)
-      // Should remove all li children from the ul. 
-    function reset(){
-        const children = ul.querySelectorAll('li') 
+// Should remove all li children from the ul. 
+// Should set background color to white.
 
-    }
-      
-      // Should set background color to white.
-      body.style.backgroundColor = 'white' 
+const button = document.getElementById("generate-color")
+const ul = document.getElementById('color-list')
+const clearButton = document.getElementById('clear')
+
+button.addEventListener('click', function () {
+  let randomColors = COLORS_ARRAY[Math.floor(Math.random() * COLORS_ARRAY.length)];
+  const body = document.querySelector('body')
+  body.style.background = randomColors
+  li.textContent = randomColors
+  li.style.backgroundColor = randomColors
+})
+let li = document.createElement('li')
+ul.appendChild(li)
+
+  const reset = () => {
+  const removeLi = ul.getElementsByTagName('li')
+  li.remove()
+  document.body.style.backgroundColor = "white"
+  }
+  clearButton.addEventListener('click', reset)
+  console.log(removeLi);
+
+
